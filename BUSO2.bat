711238840244dd082474b6dad0a3d4e94842ee3a
Arfelly Donato <arfelly23@gmail.com>
2023-12-19 13:01:22 -0500

    Se empieza con el codigo de so2 realizado

---
+++ BUSO2.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Users\monitores-ovspo\AppData\Local\Programs\Python\Python310\python.exe" V:\1DOCUMENTOS_MONITORES\CODIGOS\NovacProgram\BUSO2.py
+"C:\Users\monitores-ovspo\AppData\Local\Programs\Python\Python38\python.exe" V:\1DOCUMENTOS_MONITORES\CODIGOS\NovacProgram\BUSO2.py
+pause
