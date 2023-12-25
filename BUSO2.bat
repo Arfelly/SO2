14e3784f51c02f2a2b5a6e6a6e8a3e7a0b22e0f1
Arfelly Donato <arfelly23@gmail.com>
2023-12-25 12:43:35 -0500

    Se elimina antiguos archivos y se modifica nombre a script final

--- BUSO2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Users\monitores-ovspo\AppData\Local\Programs\Python\Python310\python.exe" V:\1DOCUMENTOS_MONITORES\CODIGOS\NovacProgram\BUSO2.py
-"C:\Users\monitores-ovspo\AppData\Local\Programs\Python\Python38\python.exe" V:\1DOCUMENTOS_MONITORES\CODIGOS\NovacProgram\BUSO2.py
-pause
